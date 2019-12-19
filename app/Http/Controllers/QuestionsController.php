c1a4d52a79ba686763d18e3b7e0ffd74dbe398b8
Azerus <kevin.marous@group-hpi.com>
2019-12-19 14:41:04 +0100

    Implement form validation

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 use App\Question;
 use Illuminate\Http\Request;
+use App\Http\Requests\AskQuestionRequest;
 class QuestionsController extends Controller
 {
     /**
@@ -30,9 +31,11 @@ class QuestionsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(AskQuestionRequest $request)
     {
-        //
+        $request->user()->questions()->create($request->only('title', 'body'));
+
+        return redirect()->route('questions.index')->with('success', "Question succesfully submitted");
     }
     /**
      * Display the specified resource.
